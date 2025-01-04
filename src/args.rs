e405b64959be9997547a69145c35f16917d562a7
Philipp Reiter <p.reiter93@gmail.con>
2025-01-04 10:27:48 +0100

    Make command arg positional

--- src/args.rs
+++ src/args.rs
@@ -4,6 +4,6 @@ use clap::Parser;
 #[command(version, about, long_about = None)]
 pub struct Args {
     /// Show the man page of a command.
-    #[arg(short, long)]
+    #[arg(value_name = "COMMAND", help = "Show the man page of a command.")]
     pub(crate) command: Option<String>,
 }
